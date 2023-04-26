ab99ddb0a5fbe08c4c0bed08c7f397e9f30217ef
naufalmms <naufalmarzuk@gmail.com>
2023-04-26 18:01:41 +0700

    onboarding

--- lib/config/theme/app_font.dart
+++ lib/config/theme/app_font.dart
@@ -1,79 +1,132 @@
 import 'package:flutter/material.dart';
-class AppFont
-{
- static  TextStyle get hero => TextStyle(
-   fontSize: 72,
-   fontWeight: FontWeight.w700,
-   color: Colors.black,
- );
- static  TextStyle get display1 => TextStyle(
-   fontSize: 56,
-   fontWeight: FontWeight.w700,
-   color: Colors.black,
- );
- static  TextStyle get display2 => TextStyle(
-   fontSize: 40,
-   fontWeight: FontWeight.w700,
-   color: Colors.black,
- );
- static  TextStyle get display3 => TextStyle(
-   fontSize: 32,
-   fontWeight: FontWeight.w700,
-   color: Colors.black,
- );
- static  TextStyle get heading1 => TextStyle(
-   fontSize: 24,
-   fontWeight: FontWeight.w700,
-   color: Colors.black,
- );
- static  TextStyle get heading2 => TextStyle(
-   fontSize: 20,
-   fontWeight: FontWeight.w700,
-   color: Colors.black,
- );
- static  TextStyle get paragraphLarge => TextStyle(
-   fontSize: 16,
-   fontWeight: FontWeight.w400,
-   color: Colors.black,
- );
- static  TextStyle get paragraphLargeBold => TextStyle(
-   fontSize: 16,
-   fontWeight: FontWeight.w700,
-   color: Colors.black,
- );
- static  TextStyle get paragraphMedium => TextStyle(
-   fontSize: 14,
-   fontWeight: FontWeight.w400,
-   color: Colors.black,
- );
- static  TextStyle get paragraphMediumBold => TextStyle(
-   fontSize: 14,
-   fontWeight: FontWeight.w700,
-   color: Colors.black,
- );
- static  TextStyle get paragraphSmall => TextStyle(
-   fontSize: 12,
-   fontWeight: FontWeight.w400,
-   color: Colors.black,
- );
- static  TextStyle get paragraphSmallBold => TextStyle(
-   fontSize: 12,
-   fontWeight: FontWeight.w700,
-   color: Colors.black,
- );
- static  TextStyle get ButtonLarge => TextStyle(
-   fontSize: 16,
-   fontWeight: FontWeight.w600,
-   color: Colors.black,
- );
- static  TextStyle get ButtonLargeMedium => TextStyle(
-   fontSize: 14,
-   fontWeight: FontWeight.w600,
-   color: Colors.black,
- );
- static  TextStyle get ButtonLargeSmall => TextStyle(
-   fontSize: 12,
-   fontWeight: FontWeight.w600,
-   color: Colors.black,
- );
+
+class AppFont {
+  static TextStyle get hero => TextStyle(
+        fontSize: 72,
+        fontWeight: FontWeight.w700,
+        color: Colors.black,
+      );
+  static TextStyle get display1 => TextStyle(
+        fontSize: 56,
+        fontWeight: FontWeight.w700,
+        color: Colors.black,
+      );
+  static TextStyle get display2 => TextStyle(
+        fontSize: 40,
+        fontWeight: FontWeight.w700,
+        color: Colors.black,
+      );
+  static TextStyle get display3 => TextStyle(
+        fontSize: 32,
+        fontWeight: FontWeight.w700,
+        color: Colors.black,
+      );
+  static TextStyle get heading1 => TextStyle(
+        fontSize: 24,
+        fontWeight: FontWeight.w700,
+        color: Colors.black,
+      );
+  static TextStyle get heading2 => TextStyle(
+        fontSize: 20,
+        fontWeight: FontWeight.w700,
+        color: Colors.black,
+      );
+  static TextStyle get paragraphLarge => TextStyle(
+        fontSize: 16,
+        fontWeight: FontWeight.w400,
+        color: Colors.black,
+      );
+  static TextStyle get paragraphLargeBold => TextStyle(
+        fontSize: 16,
+        fontWeight: FontWeight.w700,
+        color: Colors.black,
+      );
+  static TextStyle get paragraphMedium => TextStyle(
+        fontSize: 14,
+        fontWeight: FontWeight.w400,
+        color: Colors.black,
+      );
+  static TextStyle get paragraphMediumBold => TextStyle(
+        fontSize: 14,
+        fontWeight: FontWeight.w700,
+        color: Colors.black,
+      );
+  static TextStyle get paragraphSmall => TextStyle(
+        fontSize: 12,
+        fontWeight: FontWeight.w400,
+        color: Colors.black,
+      );
+  static TextStyle get paragraphSmallBold => TextStyle(
+        fontSize: 12,
+        fontWeight: FontWeight.w700,
+        color: Colors.black,
+      );
+  static TextStyle get ButtonLarge => TextStyle(
+        fontSize: 16,
+        fontWeight: FontWeight.w600,
+        color: Colors.black,
+      );
+  static TextStyle get ButtonLargeMedium => TextStyle(
+        fontSize: 14,
+        fontWeight: FontWeight.w600,
+        color: Colors.black,
+      );
+  static TextStyle get ButtonLargeSmall => TextStyle(
+        fontSize: 12,
+        fontWeight: FontWeight.w600,
+        color: Colors.black,
+      );
+
+  static TextStyle get componentLarge => const TextStyle(
+        fontSize: 16,
+        fontWeight: FontWeight.w600,
+        color: Colors.black,
+      );
+
+  static TextStyle get componentMediumBold => const TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w600,
+    color: Colors.black,
+  );
+  static TextStyle get componentMedium => const TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+    color: Colors.black,
+  );
+  static TextStyle get componentSmall => const TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.w600,
+    color: Colors.black,
+  );
+  static TextStyle get h0 => TextStyle(
+    fontSize: 72,
+    fontWeight: FontWeight.w700,
+    color: Colors.black,
+  );
+  static TextStyle get h1 => TextStyle(
+    fontSize: 56,
+    fontWeight: FontWeight.w700,
+    color: Colors.black,
+  );
+  static TextStyle get h2 => TextStyle(
+    fontSize: 40,
+    fontWeight: FontWeight.w700,
+    color: Colors.black,
+  );
+  static TextStyle get h3 => TextStyle(
+    fontSize: 32,
+    fontWeight: FontWeight.w700,
+    color: Colors.black,
+  );
+  static TextStyle get h4 => TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.w700,
+    color: Colors.black,
+  );
+  static TextStyle get h5 => TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.w700,
+    color: Colors.black,
+  );
+
 }
