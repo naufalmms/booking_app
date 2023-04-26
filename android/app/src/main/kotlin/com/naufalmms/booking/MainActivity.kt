25bcbb3b961b4a79dd40620a929407536d6e859a
naufalmms <naufalmarzuk@gmail.com>
2023-04-26 13:43:39 +0700

    Initial commit

---
+++ android/app/src/main/kotlin/com/naufalmms/booking/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.naufalmms.booking
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/config/theme/app_font.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+class AppFont
+{
+ static  TextStyle get hero => TextStyle(
+   fontSize: 72,
+   fontWeight: FontWeight.w700,
+   color: Colors.black,
+ );
+ static  TextStyle get display1 => TextStyle(
+   fontSize: 56,
+   fontWeight: FontWeight.w700,
+   color: Colors.black,
+ );
+ static  TextStyle get display2 => TextStyle(
+   fontSize: 40,
+   fontWeight: FontWeight.w700,
+   color: Colors.black,
+ );
+ static  TextStyle get display3 => TextStyle(
+   fontSize: 32,
+   fontWeight: FontWeight.w700,
+   color: Colors.black,
+ );
+ static  TextStyle get heading1 => TextStyle(
+   fontSize: 24,
+   fontWeight: FontWeight.w700,
+   color: Colors.black,
+ );
+ static  TextStyle get heading2 => TextStyle(
+   fontSize: 20,
+   fontWeight: FontWeight.w700,
+   color: Colors.black,
+ );
+ static  TextStyle get paragraphLarge => TextStyle(
+   fontSize: 16,
+   fontWeight: FontWeight.w400,
+   color: Colors.black,
+ );
+ static  TextStyle get paragraphLargeBold => TextStyle(
+   fontSize: 16,
+   fontWeight: FontWeight.w700,
+   color: Colors.black,
+ );
+ static  TextStyle get paragraphMedium => TextStyle(
+   fontSize: 14,
+   fontWeight: FontWeight.w400,
+   color: Colors.black,
+ );
+ static  TextStyle get paragraphMediumBold => TextStyle(
+   fontSize: 14,
+   fontWeight: FontWeight.w700,
+   color: Colors.black,
+ );
+ static  TextStyle get paragraphSmall => TextStyle(
+   fontSize: 12,
+   fontWeight: FontWeight.w400,
+   color: Colors.black,
+ );
+ static  TextStyle get paragraphSmallBold => TextStyle(
+   fontSize: 12,
+   fontWeight: FontWeight.w700,
+   color: Colors.black,
+ );
+ static  TextStyle get ButtonLarge => TextStyle(
+   fontSize: 16,
+   fontWeight: FontWeight.w600,
+   color: Colors.black,
+ );
+ static  TextStyle get ButtonLargeMedium => TextStyle(
+   fontSize: 14,
+   fontWeight: FontWeight.w600,
+   color: Colors.black,
+ );
+ static  TextStyle get ButtonLargeSmall => TextStyle(
+   fontSize: 12,
+   fontWeight: FontWeight.w600,
+   color: Colors.black,
+ );
+}
